aa8345ee1edcf0731fbeb49590afd0faec30b865
Egito Filho <egitofilho4@gmail.com>
2025-05-18 18:27:24 -0300

    first commit

---
+++ composeApp/src/commonTest/kotlin/com/dev/project/egito/nutrisport/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.dev.project.egito.nutrisport
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
